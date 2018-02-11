c0d12d258d6f937bf1179e0205ce4aa10b8f6c5b
Kefa7y <midomoka94@hotmail.com>
2018-02-12 01:39:24 +0200

    changed migrate and schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20180211214341) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
