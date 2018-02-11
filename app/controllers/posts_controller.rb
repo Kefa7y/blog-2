3e1d301e8ad52cf6dfd168449fc72f63d1ccf1b7
Kefa7y <midomoka94@hotmail.com>
2018-02-11 23:53:44 +0200

    modified routes and posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :update, :show, :delete]
 
-   # Index action to render all posts
    def index
      @posts = Post.all
    end
 
-   # New action for creating post
    def new
      @post = Post.new
    end
 
-   # Create action saves the post into database
    def create
      @post = Post.new
      if @post.save(post_params)
@@ -23,11 +20,9 @@ class PostsController < ApplicationController
      end
    end
 
-   # Edit action retrives the post and renders the edit page
    def edit
    end
 
-   # Update action updates the post with the new information
    def update
      if @post.update_attributes(post_params)
        flash[:notice] = "Successfully updated post!"
@@ -38,11 +33,9 @@ class PostsController < ApplicationController
      end
    end
 
-   # The show action renders the individual post after retrieving the the id
    def show
    end
 
-   # The destroy action removes the post permanently from the database
    def destroy
      if @post.destroy
        flash[:notice] = "Successfully deleted post!"
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
 
   get 'posts/show'
 
+  post 'posts/create'
+
+  post 'posts/update'
+
+  post 'posts/destroy'
+
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
