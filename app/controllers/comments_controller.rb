0f8540d5e4e410645f1ee13e6abaee4c4bf06a75
Kefa7y <midomoka94@hotmail.com>
2018-02-14 01:34:35 +0200

    added delete and edit functionality in comments, but some bugs needs to be fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,31 @@ class CommentsController < ApplicationController
     comment = Comment.create(user_id: session[:user_id], post_id: params[:post_id])
     comment.body = params[:body]
     if comment.save
-      flash[:notice] = "Successfully created user"
+      flash[:notice] = "Successfully added comment"
     else
-      flash[:alert] = "Error creating new user!"
+      flash[:alert] = "Error creating new comment!"
     end
     redirect_to controller: 'posts',action: 'show', id: params[:post_id]
   end
 
   def update
+    comment = Comment.find(params[:id])
+    if comment.update_attributes(body: params[:body])
+      flash[:notice] = "Successfully updated post!"
+    else
+      flash[:alert] = "Error updating post!"
+    end
+    redirect_to controller: 'posts',action: 'show', id: params[:post_id]
   end
 
   def delete
+    comment = Comment.find(params[:id])
+    if comment.destroy
+      flash[:notice] = "Successfully deleted comment!"
+    else
+      flash[:alert] = "Error deleting comment!"
+    end
+    redirect_to controller: 'posts',action: 'show', id: params[:post_id]
   end
 
 end
