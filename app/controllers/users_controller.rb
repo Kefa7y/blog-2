61d51f0237fd5506e8ea296dc7b33d4789d269d6
Kefa7y <midomoka94@hotmail.com>
2018-02-13 11:06:02 +0200

    Added some controller logic to users/login and to the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def signup
   end
 
@@ -6,5 +7,30 @@ class UsersController < ApplicationController
   end
 
   def profile
+    @user = session[:user]
+  end
+
+  def api_login
+    u = User.find_by email: post_params[:email]
+    if(u == null)
+      flash[:alert] = "User not found!"
+      render :login
+    else
+      if(u.password == post_params[:password])
+        session[:user] = u
+        redirect_to action: 'profile'
+      else
+        flash[:alert] = "Password not correct!"
+        render :login
+      end
+    end
+
+
+  end
+
+  private
+
+  def post_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211214341) do
+ActiveRecord::Schema.define(version: 20180213080251) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
