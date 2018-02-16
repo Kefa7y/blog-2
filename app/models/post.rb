75690edaaf0d5485733748e8f97430fa3f3fc119
Kefa7y <midomoka94@hotmail.com>
2018-02-16 22:47:44 +0200

    Modified Post model to make sure posts share tags and remove dupes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
-  has_and_belongs_to_many :tags , uniq: true
+  has_and_belongs_to_many :tags, uniq: true
 
-  def tags= (tag_list)
-   self.tags.clear
-   tag_list.strip.split(',').each do |tag|
-     self.tags.build(name: tag)
-   end
+  def tags=(tag_list)
+    tags.clear
+    tag_list.strip.split(',').each do |ctag|
+      tag = Tag.find_by(name: ctag)
+      if tag.nil?
+        tags.build(name: ctag)
+      else
+        tags << tag unless tags.include?(tag)
+      end
+    end
   end
-
 end
