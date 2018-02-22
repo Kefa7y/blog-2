5e769b27f426d5e28207fa10b396388efc4151a7
Kefa7y <midomoka94@hotmail.com>
2018-02-22 02:29:13 +0200

    added api functionality to session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,21 +7,34 @@ class SessionController < ApplicationController
     u = User.find_by email: post_params[:email]
     if(u.nil?)
       flash[:alert] = "User not found!"
-      render :new
+      respond_to do |format|
+        format.html render :new
+        format.json { render status:403 }
+      end
     else
       if(u.password == post_params[:password])
         session[:user_id] = u[:id]
-        redirect_to users_show_path
+        respond_to do |format|
+          format.html redirect_to users_show_path
+          format.json { render json:u }
+        end
       else
         flash[:alert] = "Password not correct!"
-        render :new
+
+        respond_to do |format|
+          format.html render :new
+          format.json { render status:403 }
+        end
       end
     end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to root_path
+    respond_to do |format|
+      format.html redirect_to root_path
+      format.json { render status:200 }
+    end
   end
 
   private
