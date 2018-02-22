642be857425242d0334727c14b61dff6e39cf05c
Kefa7y <midomoka94@hotmail.com>
2018-02-22 02:33:11 +0200

    Missing { } on session controller, woops

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,21 +8,21 @@ class SessionController < ApplicationController
     if(u.nil?)
       flash[:alert] = "User not found!"
       respond_to do |format|
-        format.html render :new
+        format.html { render :new }
         format.json { render status:403 }
       end
     else
       if(u.password == post_params[:password])
         session[:user_id] = u[:id]
         respond_to do |format|
-          format.html redirect_to users_show_path
+          format.html { redirect_to users_show_path }
           format.json { render json:u }
         end
       else
         flash[:alert] = "Password not correct!"
 
         respond_to do |format|
-          format.html render :new
+          format.html { render :new }
           format.json { render status:403 }
         end
       end
@@ -32,7 +32,7 @@ class SessionController < ApplicationController
   def destroy
     session.delete(:user_id)
     respond_to do |format|
-      format.html redirect_to root_path
+      format.html { redirect_to root_path }
       format.json { render status:200 }
     end
   end
